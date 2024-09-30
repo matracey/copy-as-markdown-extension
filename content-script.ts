1139093780941353e5c6ef4db271ea6b43948131
Martin Tracey <matracey@outlook.ie>
2024-09-30 16:54:50 -0700

    feat: add listener for COPY_AS_MARKDOWN messages to content script

--- content-script.ts
+++ content-script.ts
@@ -1,1 +1,9 @@
-console.log("Content script loaded...");
+import { Actions } from "./actions";
+
+{
+  chrome.runtime.onMessage.addListener(async request => {
+    if (request?.type === Actions.COPY_AS_MARKDOWN) {
+      console.log(`Received message: ${Actions.COPY_AS_MARKDOWN}.`);
+    }
+  });
+}
