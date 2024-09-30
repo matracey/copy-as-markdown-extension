7f19d6d94e1c7aab0ffd33cd1073fa35fa17305c
Martin Tracey <matracey@outlook.ie>
2024-09-30 16:56:40 -0700

    feat: add logic to fetch selected html content in content script

--- content-script.ts
+++ content-script.ts
@@ -3,7 +3,16 @@ import { Actions } from "./actions";
 {
   chrome.runtime.onMessage.addListener(async request => {
     if (request?.type === Actions.COPY_AS_MARKDOWN) {
-      console.log(`Received message: ${Actions.COPY_AS_MARKDOWN}.`);
+      const selection = window.getSelection();
+      if (!selection?.rangeCount) {
+        return;
+      }
+
+      const container = document.createElement("div");
+      for (let i = 0, len = selection.rangeCount; i < len; ++i) {
+        container.appendChild(selection.getRangeAt(i).cloneContents());
+      }
+      console.log(`Selected HTML content: ${container.innerHTML}`);
     }
   });
 }
