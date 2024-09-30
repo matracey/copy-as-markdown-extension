202194e53bea5072d5b8bc946b0e9ccdc6ddd831
Martin Tracey <matracey@outlook.ie>
2024-09-30 16:01:53 -0700

    feat: update service worker to handle click of context menu item

--- background.ts
+++ background.ts
@@ -9,4 +9,17 @@ import { Actions } from './actions';
       contexts: ["selection"],
     });
   });
+
+  chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (info.menuItemId === Actions.COPY_AS_MARKDOWN) {
+      if (!tab?.id) {
+        console.error(`Unable to determine the source tab where the ${Actions.COPY_AS_MARKDOWN} action was triggered.`);
+        return;
+      }
+
+      console.info(`Sending ${Actions.COPY_AS_MARKDOWN} message to tab: ${tab.id}`);
+      const response = await chrome.tabs.sendMessage(tab.id, { type: Actions.COPY_AS_MARKDOWN });
+      console.info("Copied as Markdown:", response?.markdownContent);
+    }
+  });
 }
