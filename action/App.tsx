cc905ea0f0dd198025dd3506b0532775b2bdb888
Martin Tracey <matracey@outlook.ie>
2024-09-30 17:27:58 -0700

    feat: update extension branding to app component

--- action/App.tsx
+++ action/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 import { Hero } from "react-daisyui";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <Hero className="py-5 px-2">
       <Hero.Content className="text-center">
         <div className="max-w-md">
-          <FontAwesomeIcon icon={faCode} size="6x" />
-          <h1 className="text-2xl font-bold">Welcome to your new extension!</h1>
+          <FontAwesomeIcon icon={faMarkdown} size="6x" transform={{ x: 2.5, y: -1.5, size: 5 }} mask={faCopy} />
+          <h1 className="text-2xl font-bold">Welcome to Copy As Markdown!</h1>
         </div>
       </Hero.Content>
     </Hero>
